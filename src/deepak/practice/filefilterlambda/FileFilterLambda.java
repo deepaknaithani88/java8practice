9cc0092436b446008c4e4b35637cf97ea145f14e
Deepak Naithani <deepaknaithani88@gmail.com>
2020-05-31 16:17:43 +0530

    updated .gitignore file

--- src/deepak/practice/filefilterlambda/FileFilterLambda.java
+++ src/deepak/practice/filefilterlambda/FileFilterLambda.java
@@ -5,16 +5,16 @@ import java.io.FileFilter;
 
 public class FileFilterLambda {
 
-	public static void main(String[] args) {
-		FileFilter filterLambda = (File file) -> file.getAbsolutePath().endsWith(".java");
+    public static void main(String[] args) {
+        FileFilter filterLambda = (File file) -> file.getAbsolutePath().endsWith(".java");
 
-		File file = new File("E:\\deepak\\eclipse-workspace\\Java8Practice\\src\\deepak\\practice\\filefilterlambda");
+        File file = new File("E:\\deepak\\eclipse-workspace\\Java8Practice\\src\\deepak\\practice\\filefilterlambda");
 
-		File[] fileArr = file.listFiles(filterLambda);
+        File[] fileArr = file.listFiles(filterLambda);
 
-		for (int i = 0; i < fileArr.length; i++) {
-			System.out.println(fileArr[i]);
-		}
-	}
+        for (int i = 0; i < fileArr.length; i++) {
+            System.out.println(fileArr[i]);
+        }
+    }
 
 }
